7a3b784af3e9c49f094c0121724d15cb94e5b334
NikBel523 <belyakov.nkt@gmail.com>
2024-03-19 16:55:54 +0300

    feat: добавлена пагинация и фильтрация для произведений.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,20 @@
 # from django.shortcuts import get_object_or_404
+
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import exceptions, filters, viewsets
 from rest_framework.pagination import PageNumberPagination
-# from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from api.serializers import (
     CategorySerializer,
     GenreSerializer,
     TitleSerializer,
 )
-
 from titles.models import Category, Genre, Title
+from api.filters import TitleFilter
+
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -46,4 +51,8 @@ class GenreViewSet(viewsets.ModelViewSet):
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+    pagination_class = PageNumberPagination
+
     # permission_classes = (IsAdminOrReadOnly, )
