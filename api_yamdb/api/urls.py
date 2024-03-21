493bd4f0333ff947619b15f7d69080761fbb0b08
NikBel523 <belyakov.nkt@gmail.com>
2024-03-21 13:35:08 +0300

    feat: comments готовы. Все тесты прозодят. Можно добавить миксинов, что бы было DRY.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 
 from api.views import (
     CategoryViewSet,
+    CommentViewSet,
     GenreViewSet,
     ReviewsViewSet,
     TitleViewSet,
@@ -31,9 +32,13 @@ v1_router.register('categories', CategoryViewSet, basename='categories')
 v1_router.register('genres', GenreViewSet, basename='genres')
 v1_router.register('titles', TitleViewSet, basename='titles')
 v1_router.register('users', UserProfileViewSet, basename='users')
+# TODO нужно применить DRY? воможно через NestedSimpleRouter
 v1_router.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewsViewSet, basename='reviews')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comments')
 
 """
 actionsMap = {
